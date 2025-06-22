7c1ae8351fc3d7a511780598c7b5336a9296239b
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2025-06-22 00:49:28 +0000

    add reusable components

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useAuth } from "@/app/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const { authenticated, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !authenticated) {
+      router.push("/login");
+    }
+  }, [loading, authenticated, router]);
+
+  if (loading || !authenticated) return <div>Loading...</div>;
+
+  return <>{children}</>;
+};
