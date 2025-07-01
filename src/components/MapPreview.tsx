8ace464a782025098378f3c93aea69ec9cfeb9ec
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2025-07-01 22:14:59 +0000

    add component for users to preview their location

---
+++ src/components/MapPreview.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useEffect } from "react";
+import {
+  useMap,
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+function RecenterMap({ lat, lng }: { lat: number; lng: number }) {
+  const map = useMap();
+
+  useEffect(() => {
+    map.setView([lat, lng]);
+  }, [lat, lng, map]);
+
+  return null;
+}
+
+export default function MapPreview({
+  lat,
+  lng,
+  onLocationChange,
+}: {
+  lat: number;
+  lng: number;
+  onLocationChange: (lat: number, lng: number) => void;
+}) {
+  const MapClickHandler = () => {
+    useMapEvents({
+      click(e) {
+        const { lat, lng } = e.latlng;
+        onLocationChange(lat, lng);
+      },
+    });
+    return null;
+  };
+
+  return (
+    <MapContainer
+      center={[lat, lng]}
+      zoom={16}
+      scrollWheelZoom={false}
+      className="h-64 w-full rounded-md z-0"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[lat, lng]}>
+        <Popup>Project Location</Popup>
+      </Marker>
+      <RecenterMap lat={lat} lng={lng} />
+      <MapClickHandler />
+    </MapContainer>
+  );
+}
