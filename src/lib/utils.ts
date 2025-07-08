a877686e242fa5477cca25e47c704d55e5fdeffa
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2025-07-08 08:25:38 +0000

    update

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// lib/utils.ts
+
+export function formatDate(dateString: string | Date): string {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+}
